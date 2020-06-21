7fd9fe907c209286d117935860d49b8f921e09f2
Marcus Lewis <mrcslws@gmail.com>
2020-06-21 12:24:14 -0700

    Store a mask; Much smaller memory footprint
    Rather than storing a list of zero indices, store a zero mask.
    
    With this change, the SparseWeightsBase no longer prescribes how the
    zeroes should be stored; the subclass gets to decide this. This
    enables subclasses to choose more efficient case-specific rezeroing
    techniques, for example when sparsity is structured (e.g.
    channel-to-channel).
    
    Memory usage of resnet50, before and after:
    
    Before:
    672,979,068 bytes total
    570,523,648 bytes of SparseWeights zero indices
    
    After:
    152,778,876 bytes total
     50,323,456 bytes of SparseWeights masks
    
    And the memory improvement gets considerably better if the sparsity is
    structured (e.g. channel to channel), enabling broadcasting.

--- nupic/torch/models/sparse_cnn.py
+++ nupic/torch/models/sparse_cnn.py
@@ -320,8 +320,8 @@ class GSCSuperSparseCNN(GSCSparseCNN):
 
 
 MODEL_URLS = {
-    "gsc_sparse_cnn": "http://public.numenta.com/pytorch/hub/gsc_sparse_cnn-eac5f79f.pth",  # noqa: E501
-    "gsc_super_sparse_cnn": "http://public.numenta.com/pytorch/hub/gsc_super_sparse_cnn-b1e1921c.pth",  # noqa: E501
+    "gsc_sparse_cnn": "http://public.numenta.com/pytorch/hub/gsc_sparse_cnn-025f3331.pth",  # noqa: E501
+    "gsc_super_sparse_cnn": "http://public.numenta.com/pytorch/hub/gsc_super_sparse_cnn-08ce1fd2.pth",  # noqa: E501
 }
 
 
--- tests/sparse_weights_test.py
+++ tests/sparse_weights_test.py
@@ -113,7 +113,7 @@ class TestSparseWeights(unittest.TestCase):
         self.assertTrue(torch.all(sw.off_mask == torch.zeros_like(sw.weight)))
         sw.weight[:] = 1
         sw.rezero_weights()
-        self.assertTrue(sw.weight.sum() == sw.weight.numel())
+        self.assertEqual(sw.weight.sum(), sw.weight.numel())
 
     def test_prunable_sparse_linear(self):
 
@@ -123,7 +123,7 @@ class TestSparseWeights(unittest.TestCase):
         self.assertTrue(torch.all(sw.off_mask == torch.ones_like(sw.weight)))
         sw.weight[:] = 1
         sw.rezero_weights()
-        self.assertTrue(sw.weight.sum() == 0)
+        self.assertEqual(sw.weight.sum(), 0)
 
     def test_prunable_dense_conv(self):
 
@@ -133,7 +133,7 @@ class TestSparseWeights(unittest.TestCase):
         self.assertTrue(torch.all(sw.off_mask == torch.zeros_like(sw.weight)))
         sw.weight[:] = 1
         sw.rezero_weights()
-        self.assertTrue(sw.weight.sum() == sw.weight.numel())
+        self.assertEqual(sw.weight.sum(), sw.weight.numel())
 
     def test_prunable_sparse_conv(self):
 
@@ -143,7 +143,7 @@ class TestSparseWeights(unittest.TestCase):
         self.assertTrue(torch.all(sw.off_mask == torch.ones_like(sw.weight)))
         sw.weight[:] = 1
         sw.rezero_weights()
-        self.assertTrue(sw.weight.sum() == 0)
+        self.assertEqual(sw.weight.sum(), 0)
 
     def test_linear_prunable_off_mask(self):
 
@@ -156,10 +156,10 @@ class TestSparseWeights(unittest.TestCase):
             [1, 1, 0, 0],
             [0, 0, 0, 0]
         ])
-        self.assertTrue(sw.sparsity == 6 / 16)
+        self.assertEqual(sw.sparsity, 6 / 16)
         sw.weight[:] = 1
         sw.rezero_weights()
-        self.assertTrue(sw.weight.sum() == 10)
+        self.assertEqual(sw.weight.sum(), 10)
 
     def test_conv_prunable_off_mask(self):
 
@@ -172,10 +172,10 @@ class TestSparseWeights(unittest.TestCase):
             [1, 1, 0, 0],
             [0, 0, 0, 0]
         ]).view(2, 2, 2, 2)
-        self.assertTrue(sw.sparsity == 6 / 16)
+        self.assertEqual(sw.sparsity, 6 / 16)
         sw.weight[:] = 1
         sw.rezero_weights()
-        self.assertTrue(sw.weight.sum() == 10)
+        self.assertEqual(sw.weight.sum(), 10)
 
 
 if __name__ == "__main__":
